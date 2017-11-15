f81f6474b0db0619ee0dbe6171e2e8ba674c56ea
Manh Le <quang.manh@gmail.com>
2017-11-15 17:37:57 -0600

    reject promise and catch exception

--- 17.Promise.js
+++ 17.Promise.js
@@ -9,6 +9,20 @@ var sundaeTicket = new Promise((res,rej)=>{
     },2000)
 })
 
+var sundaeTicket2 = new Promise((res,rej)=>{
+    setTimeout(() => {
+        rej("We're closed");
+    }, 2000);
+})
+
 // sundaeTicket.then(x=>{
 //     console.log(x)
 // })
+
+sundaeTicket2.then(()=>{
+    console.log("Not supposed to be seeing this")
+})
+.catch(ex=>{
+    console.log("Exception is",ex);
+})
+    
